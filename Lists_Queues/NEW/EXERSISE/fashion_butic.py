59212f6fd4af381387840e7547b5570c39007bee
Boris Stoilov <bstoilov81@gmail.com>
2020-12-16 12:45:45 +0200

    Add fashion buticque

---
+++ Lists_Queues/NEW/EXERSISE/fashion_butic.py
@@ -0,0 +1,22 @@
+clothes = [int(x) for x in input().split()]
+size = int(input())
+
+rack = 0
+get_clothes = 0
+
+while clothes:
+    temp = clothes[-1]
+
+    if get_clothes + temp <= size:
+        get_clothes += clothes.pop()
+    else:
+        get_clothes = 0
+        rack += 1
+
+# If have more in rack += 1
+if get_clothes > 0:
+    rack += 1
+
+print(rack)
+
+
